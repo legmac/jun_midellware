03c3f6d503e369305b464a6917b5e05519ed44c6
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2024-01-28 14:53:35 +0200

    fix

--- internal/app/mw/mw.go
+++ internal/app/mw/mw.go
@@ -2,15 +2,18 @@ package mw
 
 import (
 	"log"
+	"strings"
 
 	"github.com/labstack/echo"
 )
+
+const roleAdmin = "admin"
 
 func RoleCheck(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		val := ctx.Request().Header.Get("User-role")
 
-		if val == "admin" {
+		if strings.EqualFold(val, roleAdmin) {
 			log.Println("red button user detected")
 		}
 
